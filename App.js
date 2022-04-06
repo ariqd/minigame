3e0940aee0021aef004c0da6187bd6f357372219
Ariq Daffa <ariqdaffaathallah@gmail.com>
2022-04-06 22:51:58 +0700

    init minigame project

---
+++ App.js
@@ -0,0 +1,11 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+import StartGameScreen from './screens/StartGameScreen';
+
+const App = () => {
+  return <StartGameScreen />;
+};
+
+export default App;
+
+const styles = StyleSheet.create({});
