38a157faf5443bdfacbf51a32a89c83a6f965511
Ariq Daffa <ariqdaffaathallah@gmail.com>
2022-04-18 21:26:10 +0700

    add linear gradient and image background

--- App.js
+++ App.js
@@ -1,12 +1,19 @@
-import {StyleSheet, View} from 'react-native';
+import {StyleSheet, ImageBackground} from 'react-native';
 import React from 'react';
 import StartGameScreen from './screens/StartGameScreen';
+import LinearGradient from 'react-native-linear-gradient';
 
 const App = () => {
   return (
-    <View style={styles.appContainer}>
-      <StartGameScreen />
-    </View>
+    <LinearGradient colors={['#3b021f', '#ddb52f']} style={styles.appContainer}>
+      <ImageBackground
+        source={require('./assets/images/background.png')}
+        resizeMode="cover"
+        style={styles.appContainer}
+        imageStyle={styles.backgroundImage}>
+        <StartGameScreen />
+      </ImageBackground>
+    </LinearGradient>
   );
 };
 
@@ -15,6 +22,8 @@ export default App;
 const styles = StyleSheet.create({
   appContainer: {
     flex: 1,
-    backgroundColor: '#ddb52f',
+  },
+  backgroundImage: {
+    opacity: 0.15,
   },
 });
